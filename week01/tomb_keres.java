6b2dedf818c2d286a780de7a843ca26edb3a1955
Matyesz342 <87615537+Matyesz342@users.noreply.github.com>
2022-09-12 11:13:41 +0200

    Add files via upload

---
+++ week01/tomb_keres.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+
+public class tomb_keres
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader olvaso = new BufferedReader(new InputStreamReader(System.in));
+        int[] tomb = new int[5];
+        tomb[0] = 1;
+        tomb[1] = 2;
+        tomb[2] = 3;
+        tomb[3] = 4;
+        tomb[4] = 5;
+        System.out.print("Adjon meg egy szamot:");
+        int keresett = Integer.parseInt(olvaso.readLine());
+        for(int i=0; i<tomb.length; i++)
+        {
+            if(tomb[i] == keresett)
+            {
+                System.out.println("A keresett elem megtalalhato a tombben!");
+                System.out.println(i+1 + " hely a tombben!");
+                break;
+            }
+        }
+
+    }
+}
